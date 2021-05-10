ca7df14c4980b07abcb927165aa6ba52316da2f3
menarayanzshrestha <benarayanz@gmail.com>
2021-05-10 16:29:49 +0545

    added next/image

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,7 @@
 import Head from 'next/head';
 import Navbar from './Navbar';
+import Image from 'next/image';
+// import Gopher from "../square-gopher.png"
 
 const Layout = (props) => (
   <div>
@@ -8,7 +10,14 @@ const Layout = (props) => (
       <link rel="stylesheet" href="https://bootswatch.com/4/cerulean/bootstrap.min.css"/>
     </Head>
     <Navbar/>
+    
     <div className="container">
+        <Image
+            src='/square-gopher.png'
+            alt="Golang"
+            height={200}
+            width={500}
+        />
       {props.children}
     </div>
    
