026fc4baa73c5593897a007c368c93505a3a4c7f
menarayanzshrestha <benarayanz@gmail.com>
2021-05-10 10:19:40 +0545

    practicing with basic routes

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+import Navbar from '../components/Navbar';
+
+const About = () =>(
+    <div>
+    <Navbar/>
+        
+   
+        <h1>welome to about page</h1> 
+    </div>
+)
+
+export default About;
+
---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Navbar from '../components/Navbar';
+
+const Index = () =>(
+    <div>
+    <Navbar/>
+        
+   
+        <h1>welome to base page</h1> 
+    </div>
+)
+
+export default Index;
+
